be387fdeed0af5d2deaa6bdbb701934fb9e883bf
Benjamin Caradeuc <benjamin@caradeuc.info>
2020-05-17 04:09:00 +0200

    :art: global styles become scss

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
-import { scss } from 'svelte-preprocess';
+import svelte from 'rollup-plugin-svelte'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import scss from 'rollup-plugin-scss'
+import { scss as svelteScss } from 'svelte-preprocess'
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 
 export default {
 	input: 'src/main.js',
@@ -18,14 +19,14 @@ export default {
 		svelte({
       // preprocess svelte files with scss syntax
       preprocess: [
-        scss({})
+        svelteScss({})
       ],
 			// enable run-time checks when not in production
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write('public/build/bundle.css')
 			}
 		}),
 
@@ -38,7 +39,10 @@ export default {
 			browser: true,
 			dedupe: ['svelte']
 		}),
-		commonjs(),
+    commonjs(),
+    scss({
+      output: 'public/build/global.css'
+    }),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
@@ -47,4 +51,4 @@ export default {
 	watch: {
 		clearScreen: false
 	}
-};
+}
