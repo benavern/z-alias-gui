47493c6917a5638b5c1a9c22c6e2884134bdec18
Benjamin Caradeuc <benjamin@caradeuc.info>
2020-05-17 18:12:30 +0200

    :heavy_minus_sign: dependancies cleanup

--- src/electron.js
+++ src/electron.js
@@ -48,8 +48,10 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
-// reload when the app changes
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
-  awaitWriteFinish: true,
-});
+if (process.env.NODE_ENV === 'development') {
+  // reload when the app changes
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '../node_modules', '.bin', 'electron'),
+    awaitWriteFinish: true,
+  });
+}
